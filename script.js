ec58188c6f379c24bf460b0a6edf66431e146092
Paweł J <jpawz@outlook.com>
2023-01-29 16:54:55 +0100

    Add Book constructor, info and add functions

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.read ? "already read" : "not yet read"
+  }`;
+};
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
