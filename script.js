8c5edd9d11a052e490ca28a46312cc45338cfc18
Paweł J <jpawz@outlook.com>
2023-01-29 18:40:30 +0100

    Add addBookToLibrary and displayLibrary methods

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const myLibrary = [];
 
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -13,6 +18,16 @@ Book.prototype.info = function () {
   }`;
 };
 
-function addBookToLibrary(book) {
+function addBookToLibrary() {
+  const book = new Book(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    readInput.checked
+  );
   myLibrary.push(book);
+}
+
+function displayLibrary() {
+  myLibrary.forEach((book) => console.log(book.info()));
 }
